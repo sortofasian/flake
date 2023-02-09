be19eaecd6a374c3b9cab7e789972e81f2918b60
sortofasian <charliesyvertsen06@icloud.com>
2023-02-08 23:55:13 -0500

    Fix typo in template

--- templates/dev/flake.nix
+++ templates/dev/flake.nix
@@ -1,12 +1,12 @@
 {
     inputs.fu.url = "github:numtide/flake-utils";
-    inputs.charie.url = "github:sortofasian/flake";
+    inputs.flake.url = "github:sortofasian/flake";
     inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
 
-    outputs = { self, nixpkgs, charlie, fu }:
+    outputs = { self, nixpkgs, flake, fu }:
     fu.lib.eachDefaultSystem (system: let
         pkgs = import nixpkgs { inherit system; };
-        inherit (import charlie.lib { inherit pkgs; }) symlinkJoin;
+        inherit (import flake.lib { inherit pkgs; }) symlinkJoin;
     in {
         defaultPackage = symlinkJoin {
             name = "shell";
