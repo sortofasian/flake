c348ffda0b8df705947369f75d6cebf618525263
sortofasian <charliesyvertsen06@icloud.com>
2023-02-09 12:49:32 -0500

    Fix <S-Tab> bind for nvim

--- pkgs/neovim/config/cmp.lua
+++ pkgs/neovim/config/cmp.lua
@@ -53,7 +53,7 @@ cmp.setup({
             {"i"}),
         ["<S-Tab>"] = cmp.mapping(function (fallback)
                 if cmp.visible() then
-                    cmp.select_next_item()
+                    cmp.select_prev_item()
                 elseif luasnip.jumpable(-1) then
                     luasnip.jump(-1)
                 else
