e55f93840bdb5b37d7b53d290846b0714d27419e
sortofasian <charliesyvertsen06@icloud.com>
2023-03-13 02:32:01 -0400

    Remove options migrated to branch next

--- modules/nixos.nix
+++ modules/nixos.nix
@@ -1,49 +1,11 @@
 { pkgs, ... }:
 {
-    system.autoUpgrade = {
-        enable = true;
-        dates = "daily";
-        allowReboot = true;
-        rebootWindow.lower = "02:00";
-        rebootWindow.upper = "06:00";
-    };
-
-    nix.settings = {
-        substituters = ["https://hyprland.cachix.org"];
-        trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
-    };
-
-    boot.loader = {
-        timeout = 1;
-        efi = {
-            canTouchEfiVariables = true;
-            efiSysMountPoint = "/boot/efi";
-        };
-        systemd-boot = {
-            enable = true;
-            configurationLimit = 1;
-            consoleMode = "max";
-            editor = false;
-        };
-    };
-
-    networking = {
-    #    useDHCP = true;
-        networkmanager.enable = true;
-    };
-
-    services.journald.extraConfig = "SystemMaxUse=500M";
-
     environment.sessionVariables = {
         WINEPREFIX    = "$\{XDG_DATA_HOME}/wine";
         #XAUTHORITY    = "$XDG_RUNTIME_DIR/.Xauthority"; issue with lightdm
         XCOMPOSECACHE = "$\{XDG_CACHE_HOME}/X11/xcompose";
         XCOMPOSEFILE  = "$\{XDG_RUNTIME_DIR}/X11/xcompose";
         ERRFILE       = "$\{XDG_CACHE_HOME}/X11/xsession-errors";
-
-        __GL_SHADER_DISK_CACHE      = "1";
-        CUDA_CACHE_PATH             = "$\{XDG_CACHE_HOME}/nv";
-        __GL_SHADER_DISK_CACHE_PATH = "$\{XDG_CACHE_HOME}/nvidia";
 
     };
 
@@ -79,18 +41,8 @@
         };
     };
 
-    programs = {
-        dconf.enable = true;
-        seahorse.enable = true;
-        gnupg.agent.pinentryFlavor = "qt";
-        hyprland.enable = true;
-        hyprland.nvidiaPatches = true;
-    };
-
     services = {
-        pcscd.enable = true;
         clamav.daemon.enable = true;
         clamav.updater.enable = true;
-        gnome.gnome-keyring.enable = true;
     };
 }
