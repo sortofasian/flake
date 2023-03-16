0006f393ad0b45d1124defe15793ae2e003567bc
sortofasian <charliesyvertsen06@icloud.com>
2023-03-16 00:21:39 -0400

    Rekey secrets

--- modules/age/secrets/secrets.nix
+++ modules/age/secrets/secrets.nix
@@ -1,4 +1,6 @@
-let recipient = "age1t555ygac7r2pfnjmfxnpsunscdfarslnhrepz9jksctjvv2jysjspv5029";
-in {
-    "login.age".publicKeys = [recipient];
+let recipient = [
+    "age1t555ygac7r2pfnjmfxnpsunscdfarslnhrepz9jksctjvv2jysjspv5029"
+    "age1yubikey1qv83we6cf2k6g5t69sgafv6y4qj59pm05kz6ugyhskncnxye3rnaz0eak0f"
+]; in {
+    "login.age".publicKeys = recipient;
 }
