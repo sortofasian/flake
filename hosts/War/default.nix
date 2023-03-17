8f907dec493070af92cb248f9205d6ea479b2cc6
Charlie Syvertsen <charliesyvertsen06@icloud.com>
2023-03-17 17:25:27 -0400

    Migrate war config from main

--- hosts/War/default.nix
+++ hosts/War/default.nix
@@ -1,1 +1,19 @@
-{}
+{
+    custom = {
+        age.enable = false;
+        neovim.enable = true;
+        shell.enable = true;
+        user.name = "charlie";
+        user.home = "/Users/charlie";
+    };
+    homebrew.casks = [
+        "utm"
+        "discord"
+        "firefox"
+        "neovide"
+        "obsidian"
+
+        "unity-hub"
+        "visual-studio-code"
+    ];
+}
