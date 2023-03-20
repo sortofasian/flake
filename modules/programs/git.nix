d681962132a8e3c2642306202f37a54ce403461a
Charlie Syvertsen <charliesyvertsen06@icloud.com>
2023-03-20 12:07:39 -0400

    Configure git user with hm

--- modules/programs/git.nix
+++ modules/programs/git.nix
@@ -12,14 +12,6 @@
             "https://github.com/"
             "gh:"
         ];
-        user = {
-            name = "Charlie Syvertsen";
-            email = "charliesyvertsen06@icloud.com";
-            signingKey = switchSystem system {
-                linux = secrets.ssh.path;
-                darwin = "~/.ssh/identity.pub";
-            };
-        };
         gpg.format = "ssh";
         tag.gpgSign = true;
         commit.gpgSign = true;
@@ -29,9 +21,17 @@
         init.defaultBranch = "main";
         status.short = true;
     };
-in switchSystem system ({
+    userConfig = { user = {
+        name = "Charlie Syvertsen";
+        email = "charliesyvertsen06@icloud.com";
+        signingKey = switchSystem system {
+            linux = secrets.ssh.path;
+            darwin = "~/.ssh/identity.pub";
+        };
+    }; };
+    configText = writeText "gitconfig-system" (toGitINI config);
+    userConfigText = writeText "gitconfig-global" (toGitINI userConfig);
+in (switchSystem system ({
     linux.programs.git = { inherit config; };
-    darwin.environment.variables.GIT_CONFIG_SYSTEM = "${writeText
-        "gitconfig"
-        (toGitINI config)}";
-})
+    darwin.environment.variables.GIT_CONFIG_SYSTEM = "${configText}";
+})) // { custom.user.configFile."git/config".source = userConfigText; }
