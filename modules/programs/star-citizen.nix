a19357d5dcb2fd7e9858d8c362e2fed2fa97fdff
Charlie Syvertsen <charliesyvertsen06@icloud.com>
2023-03-20 23:49:27 -0400

    Add star citizen module

--- modules/programs/star-citizen.nix
+++ modules/programs/star-citizen.nix
@@ -2,16 +2,16 @@
     inherit (lib)
         mkIf
         types
-        mkMerge
         mkOption;
     inherit (lib.custom)
         switchSystem;
     inherit (config.custom)
+        user
         star-citizen;
 in switchSystem system { linux = {
     options.custom.star-citizen.enable = mkOption {
         type = types.bool;
-        default = false;
+        default = true;
     };
 
     config = mkIf star-citizen.enable {
@@ -26,5 +26,63 @@ in switchSystem system { linux = {
             # EAC Workaround for Star Citizen
             127.0.0.1 modules-cdn.eac-prod.on.epicgames.com
         '';
+
+        users.users.${user.name}.packages = [(pkgs.stdenv.mkDerivation (let
+            inherit (pkgs)
+                makeWrapper
+                makeDesktopItem
+                fetchFromGitHub
+                curl
+                bash
+                gnome
+                gnused
+                polkit
+                gnugrep
+                coreutils
+                findutils;
+            inherit (lib)
+                makeBinPath;
+        in rec {
+            pname = "lug-helper";
+            version = "v2.5";
+            name = "${pname}-${version}";
+            src = fetchFromGitHub {
+                owner = "starcitizen-lug";
+                repo = pname;
+                rev = version;
+                sha256 = "sha256-2ikuPqtClkvnC5dCSsf8Jt0HBKI26WY78kI+1o5w4p8=";
+            };
+
+            desktopItem = makeDesktopItem {
+                name = pname; exec = pname; icon = pname;
+                categories = ["Game"];
+                desktopName = "LUG Helper";
+                startupNotify = true;
+            };
+
+            buildInputs = [ makeWrapper ];
+
+            binPath = makeBinPath [
+                bash coreutils curl polkit
+                gnused gnome.zenity findutils gnugrep
+            ];
+
+            installPhase = ''
+                runHook preBuild
+
+                mkdir -p $out/bin
+                makeWrapper "$src/lug-helper.sh" "$out/bin/$pname" \
+                --prefix PATH : $binPath
+
+                install -Dm755 "$src/lug-logo.png" \
+                    "$out/share/pixmaps/$pname.png"
+                install -Dm755 "$src/lug-lutris-install.json" \
+                    "$out/share/$pname/lug-lutris-install.json"
+                install -Dm755 "$desktopItem/share/applications/$pname.desktop" \
+                    "$out/share/applications/$pname.desktop"
+
+                runHook postBuild
+            '';
+        }))];
     };
 }; }
