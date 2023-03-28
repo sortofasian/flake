d69725a452c11c7058ff127c5a772108e7be977b
Charlie Syvertsen <charliesyvertsen06@icloud.com>
2023-03-27 22:19:03 -0400

    Set up u2f for pam

--- modules/system/pam.nix
+++ modules/system/pam.nix
@@ -9,9 +9,12 @@
         secrets;
 in mkIf config.custom.age.enable (switchSystem system { linux = {
     # TODO: Figure out pam on darwin
-    security.pam.services = {
-        login.u2fAuth = true;
-        sudo.u2fAuth = true;
+    security.pam = {
+        u2f.enable = true;
+        u2f.cue = true;
+        u2f.origin = "pam://sortofasian";
+        services.login.u2fAuth = true;
+        services.sudo.u2fAuth = true;
     };
     custom.user.configFile."Yubico/u2f_keys"
         .source = mkOutOfStoreSymlink secrets.pam-yubikeys.path;
