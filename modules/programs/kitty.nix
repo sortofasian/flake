4d5fc7c305fd7b6ff926b2133c76b718ba6d7db7
Charlie Syvertsen <charliesyvertsen06@icloud.com>
2023-04-08 23:45:27 -0400

    Update kitty icon + ssh alias

--- modules/programs/kitty.nix
+++ modules/programs/kitty.nix
@@ -3,45 +3,53 @@
         colors;
 in {
     environment.systemPackages = [ pkgs.kitty ];
-    environment.variables.TERM = "kitty";
     environment.variables.TERMINAL = "kitty";
-    custom.user.configFile."kitty/kitty.conf".text = ''
-        font_family Fira Code Medium Nerd Font Complete
-        font_size 13.0
-        undercurl_style thick-sparse
-        cursor none
+    environment.shellAliases.ssh = "kitty +kitten ssh";
+    custom.user.configFile = {
+        "kitty/kitty.app.png".source = "${pkgs.fetchFromGitHub {
+            owner = "samholmes"; repo = "whiskers";
+            rev = "89bdb2eaa5ace843efe8ae859a670211e8246cec";
+            sha256 = "sha256-IyX100UVkSimqFAm/MqgkuUjwnaIrAaCUmK1AkWBme0=";
+        }}/whiskers-tokyo.png";
 
-        scrollback_lines 10000
-        scrollback_pager_history_size 2000
+        "kitty/kitty.conf".text = ''
+            font_family Fira Code Medium Nerd Font Complete
+            font_size 13.0
+            undercurl_style thick-sparse
+            cursor none
 
-        mouse_hide_wait 3.0
+            scrollback_lines 10000
+            scrollback_pager_history_size 2000
 
-        macos_titlebar_color #${colors.bg}
-        window_margin_width 5
+            mouse_hide_wait 3.0
 
-        foreground #${colors.fg}
-        background #${colors.bg}
-        selection_foreground none
-        selection_background none
+            macos_titlebar_color #${colors.bg}
+            window_margin_width 5
 
-        color0  #${colors.black}
-        color1  #${colors.red}
-        color2  #${colors.green}
-        color3  #${colors.yellow}
-        color4  #${colors.blue}
-        color5  #${colors.magenta}
-        color6  #${colors.cyan}
-        color7  #${colors.white}
+            foreground #${colors.fg}
+            background #${colors.bg}
+            selection_foreground none
+            selection_background none
 
-        color8  #${colors.brightblack}
-        color9  #${colors.brightred}
-        color10 #${colors.brightgreen}
-        color11 #${colors.brightyellow}
-        color12 #${colors.brightblue}
-        color13 #${colors.brightmagenta}
-        color14 #${colors.brightcyan}
-        color15 #${colors.brightwhite}
+            color0  #${colors.black}
+            color1  #${colors.red}
+            color2  #${colors.green}
+            color3  #${colors.yellow}
+            color4  #${colors.blue}
+            color5  #${colors.magenta}
+            color6  #${colors.cyan}
+            color7  #${colors.white}
 
-        background_opacity 0.85
-    '';
+            color8  #${colors.brightblack}
+            color9  #${colors.brightred}
+            color10 #${colors.brightgreen}
+            color11 #${colors.brightyellow}
+            color12 #${colors.brightblue}
+            color13 #${colors.brightmagenta}
+            color14 #${colors.brightcyan}
+            color15 #${colors.brightwhite}
+
+            background_opacity 0.85
+        '';
+    };
 }
