eef078d579e42a1f9a851e39d65a58d7b6ab7d50
Charlie Syvertsen <charliesyvertsen06@icloud.com>
2023-10-26 00:57:35 -0400

    Add devenv

--- modules/programs/direnv.nix
+++ modules/programs/direnv.nix
@@ -1,17 +1,22 @@
-{ lib, pkgs, config, ... }: let
+{ lib, pkgs, config, inputs, system, ... }: let
     inherit (lib)
         mkIf
         types
         mkOption;
     inherit (config.custom)
         direnv;
+    inherit (inputs) devenv;
 in {
     options.custom.direnv.enable = mkOption {
         type = types.bool;
         default = false;
     };
     config = mkIf direnv.enable {
-        environment.systemPackages = [ pkgs.direnv pkgs.nix-direnv ];
+        environment.systemPackages = [
+            pkgs.direnv
+            pkgs.nix-direnv
+            devenv.packages.${system}.default
+        ];
         nix.settings = {
             keep-outputs = true;
             keep-derivations = true;
